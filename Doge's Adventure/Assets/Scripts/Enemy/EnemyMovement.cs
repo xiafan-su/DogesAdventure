acf0d2da29e64fa659141f01b048a394bfec0bdd
Demonsu <245681117@qq.com>
2015-07-03 11:07:27 +0800

    tutorial finished

---
+++ Doge's Adventure/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+	Transform player;
+	PlayerHealth playerHealth;
+	EnemyHealth enemyHealth;
+	NavMeshAgent nav;
+
+	void Awake ()
+	{
+		player = GameObject.Find("Player").transform;
+		playerHealth = player.GetComponent <PlayerHealth> ();
+		enemyHealth = GetComponent <EnemyHealth> ();
+		nav = GetComponent <NavMeshAgent> ();
+	}
+
+	void Update ()
+	{
+		if (nav != null)
+		if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0) {
+				nav.SetDestination (player.position);
+		} else {
+			nav.enabled = false;
+		}
+
+	}
+}
