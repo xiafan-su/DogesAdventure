3a9c88abf01e734e34d7ba23e4933cbd7279adc8
WTIFS <da_tou@126.com>
2015-07-24 21:54:51 +0800

    add models

---
+++ Doge's Adventure/Assets/Models/Environment/DogeCoin/DogeCoinRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogeCoinRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			transform.Rotate (new Vector3 (0, 160, 0) * Time.deltaTime);
+		
+	}
+}
