f6a10398cc304d0cb1118dcc8f102b09371fd94e
Demonsu <245681117@qq.com>
2015-07-27 20:46:18 +0800

    add start menu

---
+++ Doge's Adventure/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadScene(int level)
+	{
+		Application.LoadLevel (1);
+	}
+}
