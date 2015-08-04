b151d2842929d2437af4239aef9172af06a10105
obiwandu <obiwandu@gmail.com>
2015-08-04 18:43:10 +0800

    Fix the pause menu
    pause menu may work correctly, esc to resume game,  backspace to restart
    game

--- Doge's Adventure/Assets/PauseMenu.cs
+++ Doge's Adventure/Assets/PauseMenu.cs
@@ -5,26 +5,14 @@ using System.Collections;
 public class PauseMenu : MonoBehaviour {
 	bool pauseGame = false;
 	MouseAimCamera mac;
-	Text pauseText;
-	Text restartText;
-	Button restartButton;
-	//GameObject pauseCanvas;
-	//GameObject hudCanvas;
-
-
+	GameObject pausePanel;
+	
 	// Use this for initialization
 	void Start () {
 		mac = GameObject.Find("Camera").GetComponent<MouseAimCamera> ();
-		//pauseCanvas = GameObject.Find ("PauseCanvas");
-		//hudCanvas = GameObject.Find ("HUDCanvas");
-		//pauseCanvas.SetActive (false);
-		pauseText = GameObject.Find ("PauseText").GetComponent<Text> ();
-		restartText = GameObject.Find ("RestartText").GetComponent<Text> ();
-		//restartButton = GameObject.Find ("RestartText").GetComponent<Button> ();
+		pausePanel = GameObject.Find("PausePanel");
+		pausePanel.SetActive (false);
 
-		//pauseText.enabled = false;
-		//restartText.enabled = false;
-		//restartButton.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -34,27 +22,22 @@ public class PauseMenu : MonoBehaviour {
 		}
 
 		if (pauseGame == true) {
-			Time.timeScale = 0;
-			pauseText.enabled = true;
-			restartText.enabled = true;
-			//restartButton.enabled = true;
 			mac.enabled = false;
-			//pauseCanvas.SetActive(true);
-			//hudCanvas.SetActive(false);
+			Time.timeScale = 0;
+			pausePanel.SetActive(true);
+
+			if (Input.GetKeyDown (KeyCode.Backspace) == true) {
+				restart();
+			}
 		} else {
-			Time.timeScale = 1;
-			pauseText.enabled = false;
-			restartText.enabled = false;
-			//restartButton.enabled = false;
 			mac.enabled = true;
-			//pauseCanvas.SetActive(false);
-			//hudCanvas.SetActive(true);
+			Time.timeScale = 1;
+			pausePanel.SetActive(false);
 		}
 	}
 
 	public void restart()
 	{
-		print ("restart");
 		Application.LoadLevel (0);
 	}
 }
